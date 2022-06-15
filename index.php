c7b2a3033a4e002a4cd61acd88da4082195dd0d0
Matheus Almeida <maatss12@gmail.com>
2022-06-15 09:45:49 -0300

    exemplos de select com dao

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 
 require_once("config.php");
 
-    $root = new Usuario();
+    //CARREGA APENAS UM USUARIO
+    //$root = new Usuario();
+    //$root->loadById(3);
+    //echo $root;
 
-    $root->loadById(3);
+    //CARREGA UMA LISTA DE USUARIOS.
+    //$lista = Usuario::getList();
+    //echo json_encode($lista);
 
-    echo $root;
+    //CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+    //$search = Usuario::search("jo");
+    //echo json_encode($search);
 
+    //CARREGA UM USUARIO USANDO O LOGIN E A SENHA
+
+    $usuario = new Usuario();
+    $usuario->login("jose", "1234567890");
+
+    echo $usuario;
+?>
