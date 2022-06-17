ab235efcb3803a1dd39701786b81918d07179380
Matheus Almeida <maatss12@gmail.com>
2022-06-17 09:50:57 -0300

    comando delete

--- index.php
+++ index.php
@@ -25,11 +25,15 @@ require_once("config.php");
     //$aluno->insert();
     //echo $aluno;
 
+    //Alterar um usuario
+    //$usuario = new Usuario();
+    //$usuario->loadById(8);
+    //$usuario->update("professor", "!@#$$%%");
+    //echo $usuario;
+
     $usuario = new Usuario();
-
-    $usuario->loadById(8);
-
-    $usuario->update("professor", "!@#$$%%");
+    $usuario->loadById(7);
+    $usuario->delete();
 
     echo $usuario;
 
